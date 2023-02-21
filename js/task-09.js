a700cb14ed30106984316b10191aa0da56228c59
Bartek <Barteknr1@gmail.com>
2023-02-21 21:02:46 +0100

    9, 10

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ const { log } = console;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+const changeColorButton = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', () => { 
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = body.style.backgroundColor;
+})
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
+const { log } = console;
+  document.body.style.backgroundColor = 'brown';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const create = document.querySelector('button[data-create]');
+const destroy = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+log(boxes);
+const size = 30;
+const boxCount = 0;
+
+const input = document.querySelector('input')
+
+  input.addEventListener("input", () => {
+    const value = input.value;
+    create.addEventListener('click', () => {
+        const box = document.createElement('div');
+        box.style.backgroundColor = getRandomHexColor();
+        boxes.append(box)
+        box.style.width = (size + 10 * (value - 1)) + "px"
+        box.style.height = (size + 10 * (value - 1)) + "px"
+    })
+    })
+
+destroy.addEventListener('click', () => {
+  while (boxes.firstChild) {
+    boxes.removeChild(boxes.firstChild);
+  }
+})
